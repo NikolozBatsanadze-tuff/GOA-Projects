15d1ac8cd35b55d98c9355b9cb5c385720261619
NikolozBatsanadze-tuff <nickba2009@gmail.com>
2026-05-09 23:52:57 +0400

    sixth commit

---
+++ level5/homework9.py
@@ -0,0 +1,20 @@
+age = int(input("Please enter your age: "))
+weight = int(input("Please enter your weight: "))
+
+
+if age < 10 and weight < 20:
+    print("წონა დაბალია")
+elif weight >= 20 and weight <=40 and age <10:
+    print("წონა ნორმალურია")
+elif weight > 40:
+    print("წონა მაღალია")   
+
+if age >=10 and age <= 17 and weight < 40:
+    print("წონა დაბალია")
+    
+
+
+
+
+
+
